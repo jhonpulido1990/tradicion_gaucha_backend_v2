704b1a22bc5615b1993806ebef180da9425f2071
jhonpulido1990 <3825@holbertonstudents.com>
2025-09-18 16:46:28 -0500

    feat: add new dependencies and enhance security features
    - Added dependencies for mailer, JWT, passport, and validation libraries.
    - Integrated helmet for security enhancements.
    - Configured global validation pipe with strict property checks and type transformations.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Seguridad
+  app.use(helmet());
+
+  // Configurar ValidationPipe globalmente
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // Lanza error si hay propiedades no permitidas
+      transform: true, // Transforma automáticamente los tipos
+      transformOptions: {
+        enableImplicitConversion: true, // Convierte tipos automáticamente
+      },
+    }),
+  );
 
   // Habilitar CORS
   app.enableCors({
